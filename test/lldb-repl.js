88104a814e403a2a44949f19699fcc989dc95135
jianghua.yjh <jianghua.yjh@alibaba-inc.om>
2016-08-15 11:16:01 +0800

    remote repl lldb

---
+++ test/lldb-repl.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const buf0 = new Buffer([0])
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+const spawnSync = require('child_process').spawnSync;
+
+if (process.argv.length < 3) {
+  console.log('usage: node lldb-repl.js corefile');
+  process.exit(1);
+}
+
+var env = Object.assign({}, process.env);
+
+const core = process.argv[2];
+const corerange = core + '.ranges';
+
+var range = spawnSync('readelf2segments.py', [core]);
+fs.writeFileSync(corerange, range.stdout);
+// set env
+env.LLNODE_RANGESFILE = corerange;
+
+const lldb = spawn('lldb-3.6', ['-c', core], {
+    cwd: process.cwd(),
+    env: env });
+
+
+var server = http.createServer(function (req, res) {
+  res.setHeader('content-type', 'multipart/octet-stream')
+
+  //res.write('Welcome to the Fun House\r\n')
+  req.pipe(lldb.stdin);
+  lldb.stdout.pipe(res);
+
+  // log
+  console.log(req.headers['user-agent'])
+
+  // hack to thread stdin and stdout
+  // simultaneously in curl's single thread
+  var iv = setInterval(function () {
+    res.write(buf0)
+  }, 100)
+
+  res.connection.on('end', function () {
+    clearInterval(iv)
+  })
+})
+server.listen(8000);
