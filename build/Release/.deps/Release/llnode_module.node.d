a851b877ad41efe1538bbee697e9078495d4cdb1
yjhjstz <yjhjstz@163.com>
2016-08-31 17:29:57 +0800

    remove build

--- build/Release/.deps/Release/llnode_module.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/llnode_module.node := ln -f "Release/obj.target/llnode_module.node" "Release/llnode_module.node" 2>/dev/null || (rm -rf "Release/llnode_module.node" && cp -af "Release/obj.target/llnode_module.node" "Release/llnode_module.node")
